8652f75945e44bea84ef3efeab1939708a48b987
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2023-07-30 14:16:34 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 65.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int num1=0;
+        int num2=0;
+        for(int i=0; i<num_list.length; i++){
+            if(num_list[i]%2==1){
+              num1 = num1*10+ num_list[i];
+            }else{
+               num2 = num2*10+ num_list[i];
+            }
+        }
+        answer = num1 + num2;
+        return answer;
+    }
+}
