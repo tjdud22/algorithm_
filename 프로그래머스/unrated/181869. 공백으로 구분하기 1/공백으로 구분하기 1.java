eeb38c7ccd20217f729763f1f19d4d9e7f7c332f
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2023-08-08 07:41:51 +0900

    [unrated] Title: 공백으로 구분하기 1, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String[] solution(String my_string) {
+     String[] words = my_string.split(" ");
+        return words;
+        }
+}
+        
+        
+      
+        
+        
