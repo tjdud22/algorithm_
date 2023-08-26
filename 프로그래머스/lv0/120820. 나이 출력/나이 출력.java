ccf40728efde22d4d820d2aa1d0c9184248a8bac
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2023-08-27 07:48:05 +0900

    [level 0] Title: 나이 출력, Time: 0.01 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int currentYear = 2022;
+        answer = currentYear - age+1;
+        return answer;
+    }
+}
