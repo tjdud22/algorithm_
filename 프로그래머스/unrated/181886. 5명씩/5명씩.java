0682668640637aeb55ba2a4b2acfc03e3a0849bd
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2023-11-10 11:26:51 +0900

    [level unrated] Title: 5명씩, Time: 0.21 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,15 +1,14 @@
 import java.util.Arrays;
 class Solution {
     public String[] solution(String[] names) {
-        int len = (int) Math.ceil((double) names.length / 5);
+         int len = (int) Math.ceil((double) names.length / 5);
         String[] answer = new String[len];
-        int idx = 0;
-        for (int i = 0; i < names.length; i++) {
-            if (i % 5 == 0) {
-                answer[idx++] = names[i];
-            }
+
+        for (int i = 0, idx = 0; i < names.length; i += 5) {
+            answer[idx++] = names[i];
         }
-        return Arrays.copyOf(answer, idx);
+
+        return answer;
        
     }
 }
