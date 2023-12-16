7987a716b526e7a05e852325c6a891226b1a5a11
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2023-12-17 08:46:45 +0900

    [level unrated] Title: A 강조하기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,13 +1,7 @@
 class Solution {
     public String solution(String myString) {
         String answer = myString.toLowerCase();
-        char [] charArray = answer.toCharArray();
-        for (int i = 0; i < charArray.length; i++) {
-            if (charArray[i] == 'a') {
-                charArray[i] = Character.toUpperCase(charArray[i]);
-            }
-        }
-
-        return new String(charArray);
+        answer = answer.replace('a','A');
+        return answer;
     }
 }
