df26b849db3675acbb1a10b814dc8e4e09f89a47
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-01-18 17:22:06 +0900

    [level 0] Title: 커피 심부름, Time: 0.03 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.java
@@ -2,22 +2,11 @@ import java.util.HashMap;
 class Solution {
     public int solution(String[] order) {
         int answer = 0;
-        HashMap<String, Integer> menu = new HashMap<>();
-        menu.put("iceamericano",4500);
-        menu.put("americanoice",4500);
-        menu.put("hotamericano",4500);
-        menu.put("americanohot",4500);
-        menu.put("americano",4500);
-        menu.put("anything",4500);
-        menu.put("icecafelatte",5000);
-        menu.put("cafelatteice",5000);
-        menu.put("hotcafelatte",5000);
-        menu.put("cafelattehot",5000);
-        menu.put("cafelatte",5000);
-        
-        for (int i = 0; i < order.length; i++) {
-            if (menu.containsKey(order[i])) {
-                answer += menu.get(order[i]);
+        for(int i =0; i<order.length; i++){
+            if(order[i].contains("americano") || order[i].equals("anything") ){
+                answer +=4500;
+            }else{
+                answer +=5000;
             }
         }
         return answer;
