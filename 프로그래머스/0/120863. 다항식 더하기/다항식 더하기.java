8d6707270f316ddd613db94caad05ec673a2e02b
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-01-20 08:07:18 +0900

    [level 0] Title: 다항식 더하기, Time: 7.06 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String solution(String polynomial) {
+               String answer = "";
+        int numX = 0;
+        int numY = 0;
+        String[] strList = polynomial.split("\\s*\\+\\s*");
+
+        for (String str : strList) {
+            str = str.trim(); // Trim leading and trailing whitespace
+            if (str.contains("x")) {
+                if (str.equals("x")) {
+                    numX += 1;
+                } else {
+                    numX += Integer.parseInt(str.replace("x", ""));
+                }
+            } else {
+                if (!str.isEmpty()) {
+                    numY += Integer.parseInt(str);
+                }
+            }
+        }
+
+        if (numX == 1 && numY == 0) {
+            answer = "x";
+        } else if (numX == 1 && numY != 0) {
+            answer = "x + " + numY;
+        } else if (numX != 0 && numY == 0) {
+            answer = numX + "x";
+        } else if (numX != 0 && numY != 0) {
+            answer = numX + "x + " + numY;
+        } else if (numX == 0 && numY != 0) {
+            answer = Integer.toString(numY);
+        }
+
+        return answer;
+
+    }
+}
