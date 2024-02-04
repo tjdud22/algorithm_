995d30433aa2bd3b7961ae2d9524ec1dc027cb47
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-02-04 17:43:53 +0900

    [level 0] Title: 배열 만들기 6, Time: 9.53 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+         ArrayList<Integer> answer = new ArrayList<Integer>();
+        for (int i = 0; i < arr.length; i++) {
+            if (answer.size() == 0) {
+                answer.add(arr[i]);
+            } else {
+                if (answer.get(answer.size() - 1) == arr[i]) {
+                    answer.remove(answer.size() - 1);
+                } else {
+                    answer.add(arr[i]);
+                }
+            }
+        }
+
+        if (answer.size() == 0) {
+            return new int[]{-1};
+        }
+
+        int[] result = new int[answer.size()];
+        for (int i = 0; i < answer.size(); i++) {
+            result[i] = answer.get(i);
+        }
+        return result;
+    }
+}
