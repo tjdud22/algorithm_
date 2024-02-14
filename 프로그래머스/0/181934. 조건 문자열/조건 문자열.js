97d79e281a40a863fbfcfc25a952ec2d9c292d2d
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-02-14 11:59:39 +0900

    [level 0] Title: 조건 문자열, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,13 @@
+function solution(ineq, eq, n, m) {
+    if(ineq == '<'){
+        if (eq == '=') 
+            return n<=m ? 1 : 0;
+        else if(eq == '!') 
+            return n<m ? 1 : 0;
+    }else if(ineq == '>'){
+        if (eq == '=') 
+            return n>=m ? 1 : 0;
+        else if(eq == '!') 
+            return n>m ? 1 : 0;
+    }
+}
