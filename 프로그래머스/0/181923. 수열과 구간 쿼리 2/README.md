669b7bb00a3c3c2df4f11e2ec6efc02988ad1b1d
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-02-29 01:03:38 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 3.89 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 수열과 구간 쿼리 2 - 181923 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181923?language=javascript) 
 
 ### 성능 요약
 
-메모리: 78.2 MB, 시간: 2.80 ms
+메모리: 36.9 MB, 시간: 3.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 10:56:13
+2024년 02월 29일 01:03:36
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.js
@@ -0,0 +1,13 @@
+function solution(arr, queries) {
+      var answer = [];
+    for (let i = 0; i < queries.length; i++) {
+        let min = Infinity;
+        for (let j = queries[i][0]; j <= queries[i][1]; j++) {
+            if (arr[j] > queries[i][2] && arr[j] < min) {
+                min = arr[j];
+            }
+        }
+        answer[i] = min === Infinity ? -1 : min;
+    }
+    return answer;
+}
