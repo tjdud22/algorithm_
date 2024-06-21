40196dd4e361cbcd7bc713c9c7f04521ac4604a9
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-06-22 08:36:57 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.05 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/README.md
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 빈 배열에 추가, 삭제하기 - 181860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181860?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181860) 
 
 ### 성능 요약
 
-메모리: 75.5 MB, 시간: 0.03 ms
+메모리: 76.5 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 23일 06:46:37
+2024년 06월 22일 08:37:01
 
 ### 문제 설명
 
--- 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.java
@@ -1,22 +1,22 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr, boolean[] flag) {
-          ArrayList<Integer> result = new ArrayList<Integer>();
-
-        for (int i = 0; i < flag.length; i++) {
-            if (flag[i]) {
-                for (int j = 0; j < arr[i]*2; j++) {
-                    result.add(arr[i]);
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for(int i=0; i<flag.length; i++){
+            if(flag[i]){
+                for(int j=0; j<arr[i]*2; j++){
+                result.add(arr[i]);
                 }
-            } else {
-                for (int j = 0; j < arr[i]; j++) {
-                        result.remove(result.size()-1);
+            }else{
+                for(int j=0; j<arr[i]; j++){
+                result.remove(result.size()-1);
                 }
             }
+            
         }
-
-        int answer[] = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
+        int [] answer = new int[result.size()];
+        for(int i=0; i<result.size(); i++){
             answer[i] = result.get(i);
         }
         return answer;
