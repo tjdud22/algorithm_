e2f3bff1ab8cbcdc3c7de5633f00e7246d179fa6
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-06-23 05:55:41 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 6.76 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -1,22 +1,24 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] arr, int k) {
-       ArrayList<Integer> result = new ArrayList<>();
+      ArrayList<Integer> result = new ArrayList<>();
+      
         for(int i=0; i<arr.length; i++){
-            if(!result.contains(arr[i])){
+            if(result.size() <k){
+                if(!result.contains(arr[i])){
                 result.add(arr[i]);
-            }
-             if (result.size() == k) {
-                break;
+                }
             }
         }
-           while (result.size() < k) {
+        
+        while(result.size()<k){
             result.add(-1);
         }
-        int[] answer = new int[result.size()];
-        for(int i =0; i<answer.length; i++){
+        
+        int [] answer = new int[result.size()];
+        for(int i =0; i<result.size(); i++){
             answer[i] = result.get(i);
         }
-            return answer;
+        return answer;
     }
 }
