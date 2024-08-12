84eb3f8d22d37e75a528bc6a70b91143f841bac9
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-08-12 21:32:30 +0900

    [level 0] Title: 배열 비교하기, Time: 0.22 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -10,10 +10,10 @@ function solution(arr1, arr2) {
             sum2 += num;
         }
 
-        if (arr1.length < arr2.length) {
-            return -1;
-        } else if (arr1.length > arr2.length) {
+        if (arr1.length > arr2.length) {
             return 1;
+        } else if (arr1.length < arr2.length) {
+            return -1;
         } else {
             if (sum1 < sum2) {
                 return -1;
