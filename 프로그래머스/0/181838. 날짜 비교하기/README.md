7ec235ef5af9ac9563bbc1dc1adbb3cdf3d3c102
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-08-26 21:03:52 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/README.md
+++ 프로그래머스/0/181838. 날짜 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 날짜 비교하기 - 181838 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181838) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.04 ms
+메모리: 33.4 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 12일 06:30:43
+2024년 08월 26일 21:03:57
 
 ### 문제 설명
 
--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,13 +1,13 @@
 function solution(date1, date2) {
-    let answer = 0;
-    for (let i = 0; i < date1.length; i++) {
-        if (date1[i] < date2[i]) {
-            answer = 1;
-            break;
-        } else if (date1[i] > date2[i]) {
-            answer = 0;
-            break;
-        }
+    // year, month, day를 차례로 비교합니다.
+    if (date1[0] !== date2[0]) {
+        return date1[0] < date2[0] ? 1 : 0;
     }
-    return answer;
+    if (date1[1] !== date2[1]) {
+        return date1[1] < date2[1] ? 1 : 0;
+    }
+    if (date1[2] !== date2[2]) {
+        return date1[2] < date2[2] ? 1 : 0;
+    }
+    return 0; 
 }
