5d77a9ca94e039d314eb5eb9ebe62c05740438fe
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-09-14 03:57:43 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.js
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.js
@@ -1,10 +1,3 @@
 function solution(n_str) {
-    let answer = '';
-    for(let i =0; i<n_str.length; i++){
-        if(n_str.charAt(i) != 0){
-            answer = n_str.substring(i);
-            break;
-        }
-    }
-    return answer;
+      return n_str.replace(/^0+/, '');
 }
