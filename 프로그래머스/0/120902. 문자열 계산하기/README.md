fb25833497306c1650983656e36df377b5e219b9
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-09-28 18:35:43 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120902. 문자열 계산하기/README.md
+++ 프로그래머스/0/120902. 문자열 계산하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 계산하기 - 120902 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120902) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120902?language=javascript) 
 
 ### 성능 요약
 
-메모리: 72.3 MB, 시간: 0.08 ms
+메모리: 33.4 MB, 시간: 0.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 6일 9:19:44
+2024년 09월 28일 18:35:40
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,19 @@
+function solution(my_string) {
+    let answer = 0;
+        let str = my_string.split(" ");
+
+        for (let i = 0; i < str.length; i += 2) {
+            let num = parseInt(str[i]);
+
+            if (i > 0) {
+                if (str[i - 1] === "+") {
+                    answer += num;
+                } else if (str[i - 1] === "-") {
+                    answer -= num;
+                }
+            } else {
+                answer = num;  
+            }
+        }
+        return answer;
+}
