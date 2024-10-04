c08b04b5c6b18c93609bd15ee02443ee01d15466
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2024-10-04 22:23:31 +0900

    [level 0] Title: 가위 바위 보, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,14 @@
+function solution(rsp) {
+    let answer ='';
+        for(let i =0; i <rsp.length; i++){
+                 if(rsp.charAt(i) === '2'){
+         answer +='0';}
+            else if(rsp.charAt(i) === '0'){
+          answer +='5';
+            }else{
+              answer += '2';
+            }
+        }
+        return answer;
+
+}
