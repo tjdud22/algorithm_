122d6178ef95f0af14b9426a00f11574bc3ae8b4
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-01-01 08:53:43 +0900

    [level 0] Title: n개 간격의 원소들, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/README.md
+++ 프로그래머스/0/181888. n개 간격의 원소들/README.md
@@ -1,10 +1,10 @@
 # [level 0] n개 간격의 원소들 - 181888 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181888?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181888) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.06 ms
+메모리: 33.4 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 30일 20:09:38
+2025년 01월 01일 08:53:38
 
 ### 문제 설명
 
--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.js
@@ -1,14 +1,8 @@
 function solution(num_list, n) {
-     var answer = [];
-    for (let i = 0; i < num_list.length / n; i++) {
-        answer.push(0);
-    }
-
-    var idx = 0;
-    for (let i = 0; i < num_list.length; i += n) {
-        answer[idx] += num_list[i];
-        idx++;
-    }
+    let answer = [];
+    for (let i=0; i<num_list.length; i+=n){
+        answer.push(num_list[i]);
+    }    
     return answer;
 
 }
