276c0daf0df65160e53893683d8338c1a68b09c4
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-02-06 09:55:33 +0900

    [level 0] Title: 2의 영역, Time: 0.42 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.js
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.js
@@ -1,24 +1,8 @@
 function solution(arr) {
-    let firstIdx = -1;
-    let lastIdx = -1;
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === 2) {
-            firstIdx = i;
-            break;
-        }
-    }
-
-    for (let i = arr.length - 1; i >= 0; i--) {
-        if (arr[i] === 2) {
-            lastIdx = i;
-            break;
-        }
-    }
-
-    if (firstIdx === -1 || lastIdx === -1) {
-        return [-1];
-    }
-
-    return arr.slice(firstIdx, lastIdx + 1);
+    let first = arr.indexOf(2); 
+    let last = arr.lastIndexOf(2); 
+    
+    if (first === -1) return [-1]; 
+    
+    return arr.slice(first, last + 1); 
 }
