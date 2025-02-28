43a2119827ab4e14657f8b506944900d349dd226
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-02-28 10:12:23 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.20 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,28 +1,25 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-         int lux = Integer.MAX_VALUE;
+        int lux = Integer.MAX_VALUE;
         int luy = Integer.MAX_VALUE;
         int rdx = Integer.MIN_VALUE;
         int rdy = Integer.MIN_VALUE;
         
         for(int i=0; i<wallpaper.length; i++){
-            char check[] = wallpaper[i].toCharArray();
-           
-            for(int j=0; j<check.length; j++){
-                if(check[j] == '#'){
-                     if(lux>i){
-                    lux = i;
-                }
-                if(rdx<i+1){
-                    rdx = i+1;
-                 }
+            for(int j=0; j<wallpaper[i].length(); j++){
+                if(wallpaper[i].charAt(j) == '#'){
+                    if(lux>i){
+                        lux = i;
+                    }
+                    if(rdx<i+1){
+                        rdx = i+1;
+                    }
                     if(luy >j){
                         luy = j;
                     }
                     if(rdy <j+1){
                         rdy = j+1;
                     }
-                
                 }
             }
         }
