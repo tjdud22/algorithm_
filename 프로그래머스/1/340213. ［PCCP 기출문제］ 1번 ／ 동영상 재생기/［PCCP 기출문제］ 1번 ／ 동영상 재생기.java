7828f7ed868ba2ad26917804989adcf3ccdbe34c
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-05-03 09:17:45 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.08 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,16 +1,17 @@
 class Solution {
+    
+    public int parseTime(String time){
+        String part[] = time.split(":");
+        return Integer.parseInt(part[0])*60 + Integer.parseInt(part[1]);
+    }
+    
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
         String answer = "";
-        String posPart [] = pos.split(":"); 
-        String startPart [] = op_start.split(":");
-        String endPart [] = op_end.split(":");
-        String videoPart [] = video_len.split(":");
         
-        int posNum = Integer.parseInt(posPart[0])*60 + Integer.parseInt(posPart[1]);
-        int startNum = Integer.parseInt(startPart[0])*60 + Integer.parseInt(startPart[1]);
-        int endNum = Integer.parseInt(endPart[0])*60 + Integer.parseInt(endPart[1]);
-        int videoNum = Integer.parseInt(videoPart[0])*60 + Integer.parseInt(videoPart[1]);
-        
+        int posNum = parseTime(pos);
+        int startNum = parseTime(op_start);
+        int endNum = parseTime(op_end);
+        int videoNum = parseTime(video_len);
         
         for(int i=0; i<commands.length; i++){
             if(startNum <= posNum && posNum <= endNum ){
