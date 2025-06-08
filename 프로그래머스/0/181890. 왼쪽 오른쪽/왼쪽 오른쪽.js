0a2f4b1244e7f2462f2be0daf757e4befb63cac7
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-06-08 09:52:22 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.js
@@ -1,18 +1,13 @@
 function solution(str_list) {
-    var answer = [];
-    let lIdx = "";
-    let rIdx = "";
+    let answer = [];
     
     for(let i=0; i<str_list.length; i++){
-        if(str_list[i] === "l" || str_list[i] === "r"){
             if(str_list[i] === "l"){
-                answer = str_list.slice(0,i);
-                break;
-            }else{
-                answer = str_list.slice(i+1);
-                break;
+               return str_list.slice(0,i);
+            }
+            if(str_list[i] === "r"){
+                return str_list.slice(i+1);
             }
         }
-    }
     return answer;
 }
