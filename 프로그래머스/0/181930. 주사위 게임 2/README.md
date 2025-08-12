5f8854cbced3be043f36acc3ca21d1af89c22661
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-08-13 08:48:03 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/README.md
+++ 프로그래머스/0/181930. 주사위 게임 2/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 2 - 181930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181930) 
 
 ### 성능 요약
 
-메모리: 9.3 MB, 시간: 0.00 ms
+메모리: 9.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 13일 08:36:13
+2025년 08월 13일 08:48:00
 
 ### 문제 설명
 
--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,8 +1,9 @@
 def solution(a, b, c):
+    num = a + b + c
     if a==b and b==c and c==a:
-        return (a + b + c)*(a**2 + b**2 + c**2)*(a**3 + b**3 + c**3)
+        return num*(a**2 + b**2 + c**2)*(a**3 + b**3 + c**3)
     elif a!=b and b!=c and c!=a:
-        return a + b + c
+        return num
     else:
-        return (a + b + c)*(a**2 + b**2 + c**2)
+        return num*(a**2 + b**2 + c**2)
         
