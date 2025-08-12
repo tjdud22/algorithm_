f6a5893193a11023cc79aa54152b745d17dedc47
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-08-12 10:45:06 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,9 @@
+def solution(num_list):
+    sum1 = 0
+    sum2 = 0
+    for i,num in enumerate(num_list):
+        if i % 2 == 0:
+            sum1 += num
+        else:
+            sum2 += num
+    return sum1 if sum1 > sum2 else sum2
