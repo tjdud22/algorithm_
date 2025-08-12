c7d1c763387020a7bc8bd40ee4436bae7ac18ca6
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-08-13 08:49:58 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,6 +1,6 @@
 def solution(a, b, c):
     num = a + b + c
-    if a==b and b==c and c==a:
+    if a==b and b==c:
         return num*(a**2 + b**2 + c**2)*(a**3 + b**3 + c**3)
     elif a!=b and b!=c and c!=a:
         return num
