6bb7a7366acffa317afc4d2f79507a935653226b
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-09-03 11:13:57 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/README.md
+++ 프로그래머스/0/120844. 배열 회전시키기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 회전시키기 - 120844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120844) 
 
 ### 성능 요약
 
-메모리: 9.15 MB, 시간: 0.01 ms
+메모리: 9.18 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 03일 11:08:53
+2025년 09월 03일 11:13:54
 
 ### 문제 설명
 
--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,14 +1,5 @@
 def solution(numbers, direction):
-    answer = [i for i in range(len(numbers))]
-    for (idx,num) in enumerate(numbers):
-        if direction == 'right':
-            if idx == len(numbers)-1:
-                answer[0] = num
-            else:
-                answer[idx+1] = num
-        else:
-            if idx == 0:
-                answer[len(answer)-1] = num
-            else:
-                answer[idx-1] = num    
-    return answer
+    if direction == 'right':
+        return [numbers[-1]] + numbers[:-1]
+    else:
+        return numbers[1:] + [numbers[0]]
