d28c8262088ce877d39d1ac07be3ce5c1150f535
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-10-08 04:58:22 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/README.md
+++ 프로그래머스/0/120894. 영어가 싫어요/README.md
@@ -1,10 +1,10 @@
 # [level 0] 영어가 싫어요 - 120894 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120894) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120894?language=javascript) 
 
 ### 성능 요약
 
-메모리: 91.1 MB, 시간: 0.19 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 26일 09:00:41
+2025년 10월 08일 04:58:24
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -0,0 +1,21 @@
+function solution(numbers) {
+    var answer = 0;
+        let alphabet = {
+            zero:"0",
+            one:"1",
+            two:"2",
+            three:"3",
+            four:"4",
+            five:"5",
+            six:"6",    
+            seven:"7",
+            eight:"8",
+            nine:"9"
+        }
+    
+    for(let key in alphabet){
+        numbers = numbers.replaceAll(key,alphabet[key])
+        
+    }
+    return Number(numbers);
+}
