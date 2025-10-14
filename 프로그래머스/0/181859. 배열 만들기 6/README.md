15dd88a347c0da76b82c87a81e4de6d8424edb4b
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-10-14 09:27:31 +0900

    [level 0] Title: 배열 만들기 6, Time: 28.11 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/README.md
+++ 프로그래머스/0/181859. 배열 만들기 6/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 6 - 181859 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181859?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181859) 
 
 ### 성능 요약
 
-메모리: 37.1 MB, 시간: 29.06 ms
+메모리: 63.6 MB, 시간: 28.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 14일 09:23:01
+2025년 10월 14일 09:27:29
 
 ### 문제 설명
 
--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.js
@@ -2,12 +2,10 @@ function solution(arr) {
     let answer = [];
     let i=0;
     while(i<arr.length){
-        if(answer.length == 0){
-            answer.push(arr[i++]);
-        }else if(answer.length !=0 && answer[answer.length-1] == arr[i]){
+        if(answer[answer.length-1] == arr[i]){
             answer.pop();
             i++;
-        }else if(answer.length !=0 && answer[answer.length-1] != arr[i]){
+        }else if(answer[answer.length-1] != arr[i]){
             answer.push(arr[i++]);
         }
     }
