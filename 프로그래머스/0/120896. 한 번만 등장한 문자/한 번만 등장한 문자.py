2aecd70806afbd440ae2270251398846f04d29b9
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-10-22 13:09:50 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.01 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,16 +1,10 @@
 def solution(s):
     answer = ''
-    
     str = list(s)
-    check = set(str)
     
-    for i in check:
-        cnt = 0
-        for num in str:
-            if num == i:
-                cnt += 1
-        if cnt <=1:
-            answer += i
+    for i in set(str):
+        if s.count(i) == 1:
+            answer+=i
     
     return ''.join(sorted(answer))
         
