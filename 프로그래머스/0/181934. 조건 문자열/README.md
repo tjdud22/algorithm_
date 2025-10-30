07f6af812167299504db7712884ade44981d55bd
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-10-30 09:24:29 +0900

    [level 0] Title: 조건 문자열, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181934. 조건 문자열/README.md
+++ 프로그래머스/0/181934. 조건 문자열/README.md
@@ -1,10 +1,10 @@
 # [level 0] 조건 문자열 - 181934 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181934?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181934?language=javascript) 
 
 ### 성능 요약
 
-메모리: 9.18 MB, 시간: 0.00 ms
+메모리: 33.4 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 30일 09:21:28
+2025년 10월 30일 09:24:28
 
 ### 문제 설명
 
--- 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -1,13 +1,13 @@
 function solution(ineq, eq, n, m) {
     if(ineq == '<'){
         if (eq == '=') 
-            return n<=m ? 1 : 0;
-        else if(eq == '!') 
-            return n<m ? 1 : 0;
+            return n <= m ? 1 : 0;
+        else
+            return n < m ? 1 : 0;
     }else if(ineq == '>'){
         if (eq == '=') 
-            return n>=m ? 1 : 0;
-        else if(eq == '!') 
-            return n>m ? 1 : 0;
+            return n >= m ? 1 : 0;
+        else 
+            return n > m ? 1 : 0;
     }
 }
