83839c3ee7b802ec5fdd770b8ffeda7ece5a3515
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2025-11-22 10:58:31 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,4 +1,2 @@
 def solution(s):
-    mid = len(s) // 2
-    # 파이썬 삼항연산자 : 값1 if 조건 else 값2
-    return s[mid] if len(s) % 2 == 1 else s[mid - 1:mid + 1] 
+    return s[len(s)//2-1: len(s)//2+1] if len(s) % 2 == 0 else s[len(s)//2] 
