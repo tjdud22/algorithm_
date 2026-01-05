07befe00e447ac7373a8df6a19ecfad588927ebd
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2026-01-05 11:31:31 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.07 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -2,12 +2,11 @@ def solution(n, arr1, arr2):
     answer = []
     bin1 = []
     bin2=[]
-    for i in arr1:
-        row = list(bin(i)[2:].zfill(n).replace("1","#").replace("0"," "))
-        bin1.append(row)
-    for j in arr2:
-        row = list(bin(j)[2:].zfill(n).replace("1","#").replace("0"," "))
-        bin2.append(row)
+    for i,j in zip(arr1,arr2):
+        row1 = list(bin(i)[2:].zfill(n).replace("1","#").replace("0"," "))
+        bin1.append(row1)
+        row2 = list(bin(j)[2:].zfill(n).replace("1","#").replace("0"," "))
+        bin2.append(row2)
         
     for i in range(n):
         row = ''
