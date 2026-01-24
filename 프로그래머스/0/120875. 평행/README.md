4b93a380f9b30f0f5521b170fbeebe0ca0bc3aa1
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2026-01-25 08:12:03 +0900

    [level 0] Title: 평행, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/README.md
+++ 프로그래머스/0/120875. 평행/README.md
@@ -1,10 +1,10 @@
 # [level 0] 평행 - 120875 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120875?language=javascript) 
 
 ### 성능 요약
 
-메모리: 9.21 MB, 시간: 0.01 ms
+메모리: 33.3 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 22일 16:18:43
+2026년 01월 25일 08:12:00
 
 ### 문제 설명
 
---
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -0,0 +1,13 @@
+function solution(dots) {
+    var answer = 0;
+    if((dots[0][0] - dots[1][0]) * (dots[2][1] - dots[3][1]) == (dots[0][1] - dots[1][1]) * (dots[2][0] - dots[3][0]))
+        return 1
+    
+    if((dots[0][0] - dots[2][0]) * (dots[1][1] - dots[3][1]) == (dots[0][1] - dots[2][1]) * (dots[1][0] - dots[3][0]))
+        return 1
+    
+    if((dots[0][0] - dots[3][0]) * (dots[1][1] - dots[2][1]) == (dots[0][1] - dots[3][1]) * (dots[1][0] - dots[2][0]))
+        return 1
+    
+    return answer;
+}
