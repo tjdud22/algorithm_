4ba933e65e2a1305774bc8d6f09258727c32e1e0
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2026-01-27 12:32:39 +0900

    [level 0] Title: 분수의 덧셈, Time: 13.86 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -3,8 +3,9 @@ def solution(numer1, denom1, numer2, denom2):
     denom3 = denom1*denom2
     
     num = 1
-    for i in range(2,min(numer3,denom3)+1):
+    for i in range(min(numer3,denom3),0,-1):
         if numer3 % i == 0 and denom3 % i == 0:
             num = i
+            break
     
     return [numer3//num ,denom3//num ]
