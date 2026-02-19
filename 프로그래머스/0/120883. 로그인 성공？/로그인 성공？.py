795e2038b61846a3d749a008a7db500d648f9d6e
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2026-02-19 09:05:29 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.py
@@ -1,10 +1,8 @@
 def solution(id_pw, db):
-    answer = ''
+    answer = 'fail'
     for id,pw in db:
         if id == id_pw[0] and pw == id_pw[1]:
             answer = 'login'
         elif id == id_pw[0] and pw != id_pw[1]:
             answer = 'wrong pw'
-        elif id != id_pw[0] and pw != id_pw[1]:
-            answer = 'fail'
     return answer
