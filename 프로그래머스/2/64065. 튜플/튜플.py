7c4bc269056d750342002d87549ced1617f7f60e
tjdud22 <138543367+tjdud22@users.noreply.github.com>
2026-03-21 08:43:00 +0900

    [level 2] Title: 튜플, Time: 327.51 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = []
+    s = s.split('},{')
+    s[0]=s[0][2:]
+    s[len(s)-1] = s[len(s)-1][:len(s[len(s)-1])-2]
+    s = sorted(s,key=lambda x:len(x))
+    
+    for i in s:
+        nums = list(map(int,i.split(',')))
+        for num in nums:
+            if num not in answer:
+                answer.append(int(num))
+    return answer
